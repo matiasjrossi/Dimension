75d57eb8e472c96c68cbd8d81c7c2e867e5b106b
Matías Javier Rossi <matiasjrossi@gmail.com>
2012-05-11 21:29:19 -0300

    Change random seed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <ctime>
 
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
